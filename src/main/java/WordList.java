51bb8a918fc9872e03c0e6ed03e385590f4ba44c
Sleepykevin1 <Hunterkevin12398@gmail.com>
2025-04-25 22:59:40 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -11,21 +11,20 @@ public class WordList
   public int numWordsOfLength(int len)
   {
     /* to be implemented in part (a) */
-    int wordCount = 0;
-    for(int i = 0; i<myList.size();i++){
-      if(myList.get(i).length==i)
-        wordCount++;
-    }
-    return wordCount;
+    int count = 0;
+    for(int i =0;i<myList.size();i++)
+      if(myList.get(i).length()==len)
+        count++;
+    return count;
   }
 
   public void removeWordsOfLength(int len)
   {
     /* to be implemented in part (b) */
-int wordCount = 0;
-    for(int i =0;i<myList.size();++){
-if(myList.get(i).length==len){
-  myList.remove(i);
-  i--;
+    for(int i =0;i<myList.size();i++)
+      if(myList.get(i).length()==len){
+        myList.remove(i);
+        i--;
+      }
   }
 }
